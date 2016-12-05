726c35d505fc8926415e9d35914d224a35d08a7e
Vahaagn <matgizq@gmail.com>
2016-12-05 12:42:40 +0100

    Finished Angular2 Tour-of-Heroes tutorial

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,44 +1,38 @@
-﻿import { NgModule } from '@angular/core';
+﻿import { HeroSearchComponent } from './hero-search.component';
+import './rxjs-extensions';
+
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
-
 import { HeroService } from './hero.service';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            }
-        ])
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
-    providers: [
-        HeroService
-    ],
-    bootstrap: [AppComponent]
+    providers: [ HeroService ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,40 @@
-﻿import { Component, Input } from "@angular/core";
-import { Hero } from "./hero";
+﻿import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-    selector: 'my-hero-detail',
-    template: `
-    <div *ngIf="hero">
-        <h2>{{hero.name}} details!</h2>
-        <div><label>Id: </label>{{hero.id}}</div>
-        <div>
-          <label>Name: </label>
-          <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
-      </div>
-    `
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input()
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
 }
